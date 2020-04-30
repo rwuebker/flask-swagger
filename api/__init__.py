cf8b84aab2eed9ceeae538ddd9a7fd7a89c07843
rick <richard.wuebker@accenture.com>
2020-04-30 16:41:36 -0700

    adds initial api

---
+++ api/__init__.py
@@ -0,0 +1,54 @@
+import flask
+import json
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+
+def get_record(recordId):
+    data_file_path = os.path.join(DATA_DIR, 'data.json')
+    with open(data_file_path, 'r') as fh:
+        data = json.load(fh)
+
+    record = data.get(recordId, None)
+
+    if record:
+        return {
+            recordId: record
+        }, 200
+
+    flask.abort(404, 'NotFound')
+
+
+def update_description(recordId, body):
+    print('this is body in handler: ', body)
+    body['status'] = True
+    return secondary_function(recordId)
+
+
+def secondary_function(recordId):
+    body = flask.request.json
+    print('this is body in secondary function: ', body)
+    data_file_path = os.path.join(DATA_DIR, 'data.json')
+    with open(data_file_path, 'r') as fh:
+        data = json.load(fh)
+
+    record = data.get(recordId, None)
+    if record:
+        for k, v in body.items():
+            record[k] = v
+    else:
+        return flask.abort(404, 'NotFound')
+
+    with open(data_file_path, 'w') as fh:
+        json.dump(data, fh, indent=4, sort_keys=True)
+
+    return {
+        recordId: record
+    }, 200
+
+
+if __name__ == '__main__':
+    r = get_record('123')
+    r = update_description(recordId='123', body={'description': 'new description'})
